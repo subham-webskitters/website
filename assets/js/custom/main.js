f8ae51d6c580b06466a4d0aca2638db3d1bcff0c
subham-webskitters <subham.sengupta@webskittersacademy.in>
2026-01-16 13:06:24 +0530

    Third Commit / 16.01.2026

---
+++ assets/js/custom/main.js
@@ -0,0 +1,17 @@
+// Border Carousel
+
+$('.border-carousel').owlCarousel({
+    loop:true,
+    // rewind: false,
+    autoplay: true,
+    autoplayTimeout: 0,
+    smartSpeed: 3000,
+    autoplaySpeed: 3000,
+    slideTransition: 'linear',
+    margin:10,
+    nav:false,
+    dots: false,
+    items:30
+})
+
+// Border Carousel End Here
